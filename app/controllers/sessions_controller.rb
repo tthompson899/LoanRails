28f4978f34eb873b3b57d1ac749577cc119c3141
Tiffany Thompson <tdthompson899@gmail.com>
2016-06-27 09:41:20 -0500

    Fixed login: Lender and Borrower can now login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,44 +1,26 @@
 class SessionsController < ApplicationController
-  attr_accessor :borrower
+  attr_accessor :borrower, :lender
   def index
   end
 
   def create
-    # Login is not working
-    if Borrower.where(email: params[:login][:email])
-      borrower = Borrower.where(email: params[:login][:email])
-      borrow_login
+    lender = Lender.find_by(email: params[:email])
+    borrower = Borrower.find_by(email: params[:email])
+
+    if lender && lender.authenticate(params[:password])
+      session[:user_id] = lender.id
+      redirect_to "/users/#{session[:user_id]}"
+    elsif borrower && borrower.authenticate(params[:password])
+      session[:user_id] = borrower.id
+      redirect_to "/borrowers/#{session[:user_id]}"
     else
-      Lender.where(email: params[:login][:email])
-      lender = Lender.where(email: params[:login][:email])
-      lender_login
+      flash[:errors] = "Email and Password not in database"
+      redirect_to :back
     end
   end
 
   def destroy
     reset_session
     redirect_to "/sessions"
-  end
-
-  private
-  def borrow_login
-    if borrower.authenticate(params[:login][:password])
-      session[:user_id] = borrower.id
-      redirect_to "/borrowers/#{session[:user_id]}"
-    else
-      flash[:danger] = "Invalid Credentials"
-      redirect_to :back
-    end
-  end
-
-  def lender_login
-    if lender.authenticate(params[:login][:password])
-      fail
-      session[:user_id] = lender.id
-      redirect_to "/lenders/#{session[:user_id]}"
-    else
-      flash[:danger] = "Invalid Credentials"
-      redirect_to :back
-    end
   end
 end
